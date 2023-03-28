73598c0618235e667f6f9d5c02227f84ffbf82fe
Admin <admin@MacBook-Pro-Admin.local>
2023-03-28 09:08:53 +0300

    create dir unittest and create strtolist.py file

---
+++ unittest/main.py
@@ -0,0 +1,15 @@
+import unittest
+
+def str_list(string):
+    list1 = string.split()
+    return list1
+
+class Test(unittest.TestCase):
+
+    def test_str_list(self):
+        self.assertEqual(str_list("as sa"), ["as","sa"])
+        self.assertEqual(str_list(""), [" "])
+        self.assertEqual(str_list("asdwqd dqwdqd dqwqdq wdwq sa"), ["asdwqd","dqwdqd", "dqwqdq", "wdwq", "sa"])
+        
+    
+
